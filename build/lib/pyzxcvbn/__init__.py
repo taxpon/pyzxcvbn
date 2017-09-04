6927eedadc08b0c91a07a4a955ad9917749c855b
Takuro Wada <taxpon@gmail.com>
2017-09-04 11:26:40 +0900

    chore: release 0.6.0

--- build/lib/pyzxcvbn/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from main import zxcvbn
-
-__title__ = "pyzxcvbn"
-__version__ = "0.5.0"
-__author__ = "Takuro Wada"
-__author_email__ = "taxpon@gmail.com"
-__url__ = "https://github.com/taxpon/pyzxcvbn"
-__license__ = "MIT"
--- pyzxcvbn/__init__.py
+++ pyzxcvbn/__init__.py
@@ -2,7 +2,7 @@
 from .main import zxcvbn
 
 __title__ = "pyzxcvbn"
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 __author__ = "Takuro Wada"
 __author_email__ = "taxpon@gmail.com"
 __url__ = "https://github.com/taxpon/pyzxcvbn"
